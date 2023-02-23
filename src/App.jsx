09b7acd667348b6da8d1b732023a1b7e7155ea63
Daria <dariastoicad@gmail.com>
2023-02-23 15:05:31 +0100

    removing items

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
+import "./index.css";
 
 function App() {
-  const [tasks, setTasks] = useState(["write email", "do groceries"]);
+  const [tasks, setTasks] = useState([]);
 
   const [input, setInput] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setTasks([input, ...tasks]);
+    setInput("");
   };
+
+  const removeTask = (index) => {
+    const newList = tasks.filter((_, taskIndex) => taskIndex !== index);
+    setTasks(newList);
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -23,8 +31,13 @@ function App() {
       </form>
 
       <ul>
-        {tasks.map((task) => {
-          return <li>{task}</li>;
+        {tasks.map((task, index) => {
+          return (
+            <div className="flex" key={index}>
+              <li>{task}</li>
+              <button onClick={() => removeTask(index)}>remove</button>
+            </div>
+          );
         })}
       </ul>
     </div>
